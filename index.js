b6b02b64ec64e847240a1e688b272ad5165e2529
Amy Zhou <aymeighz@gmail.com>
2018-07-18 14:04:49 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,29 @@
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name);
+  return "Welcome," + name + " you are number" + katzDeliLine.length + " in line.";
+  
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0)
+  conso "There is nobody waiting to be served!";
+} else {
+  return "Currently serving " + katzDeliLine.shift() + ".";
+}
+
+function currentLine(katzDeliLine) {
+  if(katzDeliLine.length === 0)
+  return "The line is currently empty."
+} else {
+  var line = [];
+  let i=0;
+  while (i<katzDeliLine.length) {
+    line.push(" " + [i+1] + "." + katzDeliLine[i]);
+    i++;
+  }
+  return "The line is currently:" + line;
+}
+}
+  }
+}
+}
